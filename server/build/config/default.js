ff773b2644711b5358aaa8aec76558abf1d462dc
Stephanie Egbuonu <stephanieegbuonu@gmail.com>
2023-06-07 19:11:00 +0100

    works

--- server/build/config/default.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 4000,
-    dbUri: "mongodb://127.0.0.1:27017/url-shortener",
-};
--- server/build/src/schemas/createShortUrl.schema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const yup_1 = require("yup");
-exports.default = (0, yup_1.object)({
-    destination: (0, yup_1.string)().required("Destination is required"),
-});
